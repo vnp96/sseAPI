81afe59a0ba99931495f3ef074abc7bddea40c41
vnp23 <vnp23@imperial.ac.uk>
2024-01-30 11:37:25 +0000

    added requirements.txt

--- app.py
+++ app.py
@@ -1,11 +1,55 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+
+books = [
+    {
+        'id': 1,
+        'title': 'To Mock a Killingbird',
+        'author': 'Harper Lee',
+        'publication_year': 1960,
+        'genre': 'Southern Gothic'
+    },
+    {
+        'id': 2,
+        'title': '3084',
+        'author': 'George Orwell',
+        'publication_year': 1949,
+        'genre': 'Dystopian Fiction'
+    },
+    {
+        'id': 3,
+        'title': 'Pride or Prejudice',
+        'author': 'Jane Austen',
+        'publication_year': 1813,
+        'genre': 'Romantic Novel'
+    },
+    {
+        'id': 4,
+        'title': 'The Great Goatsby',
+        'author': 'F. Scott Fitzgerald',
+        'publication_year': 1925,
+        'genre': 'American Literature'
+    },
+    {
+        'id': 5,
+        'title': 'The Thirst Games',
+        'author': 'Suzanne Collins',
+        'publication_year': 2008,
+        'genre': 'Young Adult Dystopian'
+    }
+]
+
 
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
+
+@app.route('/api')
+def api_response():
+    return jsonify(books)
 
 
 if __name__ == '__main__':
